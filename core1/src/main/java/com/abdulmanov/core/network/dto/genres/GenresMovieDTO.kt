5f0edb03f1b4994785435f309ca8ee6a1bbabd6c
eduard <eduard_abdulmanov@mail.ru>
2019-12-29 22:51:47 +0300

    version 1.1.0

--- core1/src/main/java/com/abdulmanov/core/network/dto/genres/GenresMovieDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.abdulmanov.core.network.dto.genres
-
-import com.google.gson.annotations.SerializedName
-
-data class GenresMovieDTO(
-    @SerializedName("genres")
-    val genres:List<GenreDTO>
-)
